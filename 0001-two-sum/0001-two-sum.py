cb02f01ae0e35ecadb665de40a76442e84d11d11
Eunji (Silvia) Kwon <sandeul97@gmail.com>
2024-05-31 12:57:39 -0400

    Time: 2116 ms (42.68%), Space: 12.6 MB (47.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+
+
+
+        
